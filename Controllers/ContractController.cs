95bf755db58b570a471318299f4e72523d08ec7a
Ali Raza <aliraza1567@gmail.com>
2017-10-26 00:38:16 +0200

    Web API 2, Dependency Injection with Ninject

---
+++ Controllers/ContractController.cs
@@ -0,0 +1,54 @@
+﻿using DutchCourse.Database.Entities;
+using DutchCourse.Database.Repositories;
+using DutchCourse.WebApi.Models;
+using System;
+using System.Web.Http;
+
+namespace DutchCourse.WebApi.Controllers
+{
+    public class ContractController : ApiController
+    {
+        private readonly IContract _contract;
+
+        public ContractController(IContract contract)
+        {
+            _contract = contract;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] ContractModel contractModel)
+        {
+            if (contractModel == null)
+                return NotFound();
+
+            var contractEntity = new ContractEntity
+            {
+                ContractDate = DateTime.UtcNow,
+                AdminCost = contractModel.AdminCost,
+                CostPerHour = contractModel.CostPerHour,
+                LesMaterialCost = contractModel.LesMaterialCost,
+                TotalCost = contractModel.TotalCost,
+                TotalHours = contractModel.TotalHours
+            };
+
+            _contract.Add(contractEntity);
+
+            return Ok(new GenericResult() {Success = true});
+        }
+
+        [HttpGet]
+        [Route("GetOne")]
+        public IHttpActionResult GetOne([FromBody]int contractId)
+        {
+            var result = _contract.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var result = _contract.GetAll();
+            return Ok(result);
+        }
+    }
+}
---
+++ Models/ContractModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DutchCourse.WebApi.Models
+{
+    public class ContractModel
+    {
+        public decimal TotalHours { get; set; }
+        public decimal LesMaterialCost { get; set; }
+        public decimal AdminCost { get; set; }
+        public decimal TotalCost { get; set; }
+        public decimal CostPerHour { get; set; }
+        public DateTime ContractDate { get; set; }
+    }
+}
